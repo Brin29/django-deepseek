de5ac0b72bf2c15e363c0d5c85884f5ec03b5ef3
Brin29 <breinerstivenparracortes50@gmail.com>
2025-02-19 14:41:02 -0500

    cambios

--- api/models.py
+++ api/models.py
@@ -1,6 +1,1 @@
 from django.db import models
-
-# Create your models here.
-class Chat(models.Model):
-    message = models.TextField()
-    response = models.TextField()
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,2 @@
 from django.urls import path
 from . import views
-
-# router=routers.DefaultRouter()
-
-urlpatterns=[
-    # path('', include(router.urls))
-    path('', views.chatbot, name='chatbot')
-]
